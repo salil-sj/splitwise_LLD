bc282ff4f3bd1017c537cdaf3cfec489e63e844f
saliljoshi <salilsj1619@gmail.com>
2025-04-18 17:27:17 +0530

    Project Setup

---
+++ src/main/java/com/splitwise/LLD/SplitwiseLLD/SplitwiseLldApplication.java
@@ -0,0 +1,13 @@
+package com.splitwise.LLD.SplitwiseLLD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseLldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitwiseLldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/splitwise/LLD/SplitwiseLLD/SplitwiseLldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitwise.LLD.SplitwiseLLD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseLldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
